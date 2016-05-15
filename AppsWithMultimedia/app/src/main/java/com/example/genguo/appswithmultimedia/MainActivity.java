4f0c8a9edcbd0142ffb0a1d9fdb0b1cf49fa78d6
Kingo Wang <genguo@genguo-macbookpro.roam.corp.google.com>
2016-05-15 15:51:30 +1000

    fmt

--- AppsWithMultimedia/app/src/main/java/com/example/genguo/appswithmultimedia/MainActivity.java
+++ AppsWithMultimedia/app/src/main/java/com/example/genguo/appswithmultimedia/MainActivity.java
@@ -1,19 +1,37 @@
 package com.example.genguo.appswithmultimedia;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.VideoView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int TAKE_PHOTO_CODE = 10;
+    private static final int CAPTURE_VIDEO_CODE = 11;
+    private ImageView imageView;
+    private VideoView videoView;
+    private String imageFilePath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +50,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MusicService.class);
         intent.setAction(MusicService.ACTION_PLAY);
         startService(intent);
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        imageView = (ImageView)findViewById(R.id.imageView);
+        videoView = (VideoView)findViewById(R.id.videoView);
     }
 
     @Override
@@ -64,5 +85,93 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void takePhoto(View v){
+        Log.d("kingo","take photo");
+        Intent intent = new Intent();
+        intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            File image = null;
+            try{
+                image = createImageFile();
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+            if(image!=null){
+                Log.d("kingo", " file"+image.getAbsolutePath());
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,Uri.fromFile(image));
+            }
+            startActivityForResult(intent,TAKE_PHOTO_CODE);
+        }
+    }
+
+    public void captureVideo(View v){
+        Log.d("Kingo", "capture video");
+        Intent intent = new Intent();
+        intent.setAction(MediaStore.ACTION_VIDEO_CAPTURE);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent, CAPTURE_VIDEO_CODE);
+        }
+    }
+
+    public void useCamera(View v){
+        Intent intent = new Intent(this, UseCameraAcivity.class);
+        startActivity(intent);
+    }
+
+    private File createImageFile() throws IOException{
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String name = "JPEG_"+timestamp+"_";
+        File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(name,".jpg",dir);
+        imageFilePath = image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d("kingo","on activity result");
+        if(requestCode == TAKE_PHOTO_CODE && resultCode == RESULT_OK){
+            Bundle extra = data.getExtras();
+            if(extra != null) {
+                Bitmap bitmap = (Bitmap) extra.get("data");
+                imageView.setImageBitmap(bitmap);
+            }
+            else {
+                setPic();
+            }
+        }
+        else if(requestCode == CAPTURE_VIDEO_CODE && resultCode == RESULT_OK){
+            Uri uri = data.getData();
+            Log.d("kingo","on video result:"+uri);
+            videoView.setVideoURI(uri);
+            videoView.start();
+        }
+    }
+
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = 200;
+        int targetH = 200;
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(imageFilePath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(imageFilePath, bmOptions);
+        imageView.setImageBitmap(bitmap);
     }
 }
--- AppsWithMultimedia/app/src/main/java/com/example/genguo/appswithmultimedia/MusicService.java
+++ AppsWithMultimedia/app/src/main/java/com/example/genguo/appswithmultimedia/MusicService.java
@@ -1,5 +1,7 @@
 package com.example.genguo.appswithmultimedia;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.media.AudioManager;
@@ -20,6 +22,16 @@ public class MusicService extends Service implements MediaPlayer.OnPreparedListe
     private static final String [] URLS = {"http://genguowang.github.io/music/little_lucky.mp3"
             ,"http://genguowang.github.io/music/bu_jiang_jiu.mp3"};
 
+    private int status;
+    private static final int STATUS_IDLE = 0;
+    private static final int STATUS_PLAY = 1;
+    private static final int STATUS_PAUSE = 2;
+
+    private int mp_status;
+    private static final int MP_IDLE = 0;
+    private static final int MP_PREPARED = 1;
+    private Notification.Builder notification;
+
     private void initMediaPlayer(){
         mediaPlayer = new MediaPlayer();
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
@@ -34,22 +46,52 @@ public class MusicService extends Service implements MediaPlayer.OnPreparedListe
     }
 
     @Override
+    public void onCreate() {
+        status = STATUS_IDLE;
+        mp_status = MP_IDLE;
+        initMediaPlayer();
+        String soneName = "no song";
+        PendingIntent pi = PendingIntent.getActivity(getApplicationContext(),0,new Intent(getApplicationContext(),MainActivity.class),PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification.Builder builder = new Notification.Builder(getApplicationContext());
+        builder.setTicker("wgg ticker text");
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setOngoing(true);
+        builder.setContentIntent(pi);
+        builder.setContentText("paused");
+        builder.setContentTitle("Kingo Player");
+        notification = builder;
+
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        mediaPlayer.release();
+        mediaPlayer = null;
+        super.onDestroy();
+    }
+
+    @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         if(intent == null){
             Log.d("Kingo", "start null");
-            initMediaPlayer();
+            status = STATUS_PLAY;
             return START_STICKY;
         }
         Log.d("Kingo","on start command");
         if(intent.getAction().equals(ACTION_PLAY)){
-            initMediaPlayer();
+            status = STATUS_PLAY;
         }
         return START_STICKY;
     }
 
     @Override
     public void onPrepared(MediaPlayer mp) {
-        mp.start();
+        if(status == STATUS_PLAY) {
+            mp.start();
+            notification.setContentText("A little luck");
+            startForeground(11, notification.build());
+        }
     }
 
     @Override
@@ -62,5 +104,32 @@ public class MusicService extends Service implements MediaPlayer.OnPreparedListe
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    private void changeState(int nextStatus){
+        if(mediaPlayer == null) return;
+        status = nextStatus;
+        switch (nextStatus){
+            case STATUS_PLAY:
+                if(mp_status == MP_PREPARED){
+                    mediaPlayer.start();
+                    notification.setContentText("A little luck");
+                    startForeground(11, notification.build());
+                }
+                else
+                {
+                    mediaPlayer.prepareAsync();
+                }
+                break;
+            case STATUS_IDLE:
+                mediaPlayer.stop();
+                stopForeground(true);
+                mp_status = MP_IDLE;
+                break;
+            case STATUS_PAUSE:
+                stopForeground(true);
+                mediaPlayer.pause();
+                break;
+        }
     }
 }
